76bd41de0be57493948b26d6bd4104cf57f04390
mecher_l_home <maximchernyak98@gmail.com>
2020-09-19 13:32:50 +0300

    finish with getattr

--- handlers/planet_dialogue.py
+++ handlers/planet_dialogue.py
@@ -4,20 +4,32 @@ import ephem
 
 
 def planet_dialogue(update, context):
+
     date_now = datetime.date.today().strftime("%y/%m/%d")
-    planet_for_search = ephem.Planet
-    planet_name = update.message.text.split('/planet')[1].capitalize().strip()
+    planet_name_b = update.message.text.split('/planet')[1].strip()
+    planet_name = planet_name_b.capitalize()
+    planets_list = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ]
     # Search planet in planet list
-    try:
-        planet = getattr(ephem, planet_name)
-        if planet:
-            if planet == ephem.Earth():
-                update.message.reply_text(
-                    'We are on Earth, from our point of view, '
-                    'we are not in a constellation')
-            else:
-                planet_for_search = planet
-    except AttributeError:
+    if planet_name in planets_list:
+        if planet_name == 'Earth':
+            update.message.reply_text('We are on Earth, from our point of view, '
+                                      'we are not in a constellation')
+        else:
+            try:
+                planet_for_search = getattr(ephem, planet_name)()
+            except AttributeError:
+                update.message.reply_text('Check planet name')
+                return
+    else:
         update.message.reply_text('Planet not found')
         return
     # calculate actual constellation fot planet
